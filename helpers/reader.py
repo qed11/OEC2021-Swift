0175abd8e2a7d76d465565396261d0de586e74b0
eqd11 <ericdai1130@gmail.com>
2021-01-23 15:13:36 -0500

    reader function

--- helpers/reader.py
+++ helpers/reader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from helpers.people import Person
+from people import Person
 
 def getDic(filename:str):
     #big dictionary holding all information
@@ -10,18 +10,19 @@ def getDic(filename:str):
     inf = pd.read_excel(filename,sheet_name = 'ZBY1 Status', nrows = 4).to_numpy()
     
     #read student information first 
-    stu = pd.read_excel(filename,sheet_name = 'Student Records', nrows = 580).to_numpy()
+    stu = pd.read_excel(filename,sheet_name = 'Student Records').to_numpy()
     for i in stu:
         person_dict = dict()
-        if i[0] == np.nan:
+        if np.isnan(i[0]) == True:
             break
         #generate identifier
+        i[0] = int(i[0])
         key = 'S-' + str(i[0])
         #extracurricular
         ext = extra(str(i[9]))
         #health
         health = 0
-        if i[8] != np.nan:
+        if np.isnan(i[8]) == False:
             health = 1
         #creating individual element
         for j in inf:
@@ -38,15 +39,17 @@ def getDic(filename:str):
         person_dict['heatlh'] = health
         person_dict['excur'] = ext
         person = Person.from_dict(person_dict)
+        #print(key)
         person_class[key] = person
 
     
     #teachers
-    t = pd.read_excel(filename,sheet_name = 'Teacher Records',nrows = 20).to_numpy()
+    t = pd.read_excel(filename,sheet_name = 'Teacher Records').to_numpy()
     for i in t:
         person_dict = dict()
-        if i[0] == np.nan:
+        if np.isnan(i[0]) == True:
             break
+        i[0] = int(i[0])
         key = 'T-'+str(i[0])
         for j in inf:
             if j[0] == i[0] and j[1] == i[1] and j[2] == i[2]:
@@ -56,16 +59,17 @@ def getDic(filename:str):
         person_dict['p1'] = i[3]
         person_dict['ident'] = key
         person = Person.from_dict(person_dict)
+        #print(key)
         person_class[key] = person
     
     #TAs
-    ta = pd.read_excel(filename, sheet_name = 'Teaching Assistant Records',nrows = 6).to_numpy()
+    ta = pd.read_excel(filename, sheet_name = 'Teaching Assistant Records').to_numpy()
     #print(ta)
     for i in ta:
+        if np.isnan(i[0]) == True:
+            break
         person_dict = dict()
         i[0] = int(i[0])
-        if i[0] == np.nan:
-            break
         key = 'TA-'+str(i[0])
         for j in inf:
             if j[0] == i[0] and j[1] == i[1] and j[2] == i[2]:
@@ -78,6 +82,7 @@ def getDic(filename:str):
         person_dict['p4'] = i[6]
         person_dict['ident'] = key
         person = Person.from_dict(person_dict)
+        #print(key)
         person_class[key] = person
 
     return person_class
@@ -94,18 +99,20 @@ def extra(el):
         #one extracurricular
         return [el]
 
+"""
 if __name__=='__main__':
     p_class = getDic('OEC2021 - School Record Book .xlsx')
     '''
-    print(p_class['S-86'].l_name)
-    print(p_class['S-86'].f_name)
-    print(p_class['S-86'].ident)
-    print(p_class['S-86'].infected)
-    print(p_class['S-86'].excur)
-    print(p_class['S-86'].p1)
-    print(p_class['S-86'].p2)
-    print(p_class['S-86'].p3)
-    print(p_class['S-86'].p4)
-    print(p_class['S-86'].health)    
-    print(p_class['S-86'].grade)
+    print(p_class['S-580'].l_name)
+    print(p_class['S-580'].f_name)
+    print(p_class['S-580'].ident)
+    print(p_class['S-580'].infected)
+    print(p_class['S-580'].excur)
+    print(p_class['S-580'].p1)
+    print(p_class['S-580'].p2)
+    print(p_class['S-580'].p3)
+    print(p_class['S-580'].p4)
+    print(p_class['S-580'].health)    
+    print(p_class['S-580'].grade)
     '''
+"""
