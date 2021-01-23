ab10ca83b3e8780c8490ebc5efb05cdeff6cd896
James Liang <james.liangyy@gmail.com>
2021-01-23 16:13:42 -0500

    Main changes, transitionInfection, updateClassrooms

--- helpers/group.py
+++ helpers/group.py
@@ -9,18 +9,58 @@ class LunchGroup:
 
 class Classroom:
 
-    def __init__(self, name, lastPeriod, currentPeriod, baseEnvRate=0):
+    def __init__(self, lastPeriod, currentPeriod, baseEnvRate=0):
         '''
         Location class used to represent classrooms and extracurricular locations
 
-        :param name: Name of classroom/location
         :param lastPeriod: list of student ids within this classroom in the last period
         :param currentPeriod: list of student ids within this classroom for the current period
         :param baseEnvRate: infection rate of classroom due to inhabitation of infectious individuals
         '''
 
-        self.name = name
         self.lastPeriod = lastPeriod
         self.currentPeriod = currentPeriod
         self.baseEnvRate = baseEnvRate
         self.infectedList = None
+        self.lastInfected = None
+
+def updateClassrooms(classrooms, info_dict, period):
+    '''
+    Method for updating classroom information based on data
+
+    :param classrooms: dictionary of classroom objects with keys corresponding to names
+    :param info_dict: dictionary of Person objects from data with keys corresponding to student ids
+    :param period: current period in simulation
+    '''
+
+    class_names = ['Physics', 'Biology', 'Functions', 'Calculus', 'Philosophy', 'Art', 'Drama', 'Computer Science',
+                   'Computer Engineering', 'Humanities']
+
+    if classrooms == None:
+        classrooms = {}
+        for name in class_names:
+            for letter in ['A', 'B']:
+                in_class = []
+                classroom_name = name+' '+letter
+                classrooms[classroom_name] = Classroom([], None, 0)
+                # No last period for the first period
+
+            for student_id in info_dict.keys():
+                classrooms[info_dict[student_id].p1].currentPeriod.append(student_id)
+                if info_dict[student_id].infected:
+                    classrooms[info_dict[student_id].p1].infectedList.append(student_id)
+
+    else:
+        for name in classrooms.keys():
+            classrooms[name].currentPeriod = []
+            classrooms[name].lastPeriod = []
+            classrooms[name].infectedList = []
+        for student_id in info_dict.keys():
+            student_periods = [info_dict[student_id].p1, info_dict[student_id].p2, info_dict[student_id].p3,
+                               info_dict[student_id].p4]
+
+            classrooms[student_periods[period-1]].currentPeriod.append(student_id)
+            classrooms[student_periods[period-2]].lastPeriod.append(student_id)
+            if info_dict[student_id].infected:
+                classrooms[student_periods[period-1]].infectedList.append(student_id)
+                classrooms[student_periods[period-2]].infectedList.append(student_id)
--- helpers/infection.py
+++ helpers/infection.py
@@ -4,7 +4,7 @@ import random
 def infectPeriod(infoDict, classrooms, infectedList, baseRate, baseEnvRate):
 
     '''
-    Method that updates infoDict with newly infected students - intended to be used every period
+    Method that returns a list of students to be infected - called every period
 
     :param infoDict: Dictionary with student ids as keys, and Person class as associated values
     :param classrooms: List of classroom classes for students in the current period
@@ -40,7 +40,53 @@ def infectPeriod(infoDict, classrooms, infectedList, baseRate, baseEnvRate):
                     if randomSample < infectProb:
                         toInfect.add(student)
 
-        classroom.baseEnvRate = baseEnvRate*len(classroom.infectedList)
+        classroom.baseEnvRate = baseEnvRate * len(classroom.infectedList)
+
+        toInfect = list(toInfect)
+        newlyInfected += len(toInfect)
+
+        # Update list of people to be infected next period
+        nextPeriod.extend(toInfect)
+
+    return nextPeriod, newlyInfected / len(infectedList)
+
+
+def infectTransition(infoDict, classrooms, infectedList, baseRate):
+
+    '''
+    infectPeriod sample that should use lower baseRate, and uses lastPeriod in consideration of infecting others
+
+    :param infoDict: Dictionary with student ids as keys, and Person class as associated values
+    :param classrooms: List of classroom classes for students in the current period
+    :param infectedList: List of student ids pertaining to known infectious students
+    :param baseRate: Base rate at which an infected individual will infect others
+
+    :return list: List of student ids that represent students newly infected this period
+    :return int: Total number of newly infected individuals over total number of infected individuals at period start
+    '''
+
+    newlyInfected = 0
+    nextPeriod = []
+
+    for classroom in classrooms:
+
+        # A set is used to prevent redundant entries
+        toInfect = {}
+        for _ in classroom.infectedList:
+            for student in classroom.lastPeriod:
+                if student not in classroom.lastInfected:
+                    randomSample = random.uniform(0, 1)
+                    infectProb = augmentProb(baseRate, infoDict[student])
+                    if randomSample < infectProb:
+                        toInfect.add(student)
+
+        for _ in classroom.lastInfected:
+            for student in classroom.currentPeriod:
+                if student not in classroom.infectedList:
+                    randomSample = random.uniform(0, 1)
+                    infectProb = augmentProb(baseRate, infoDict[student])
+                    if randomSample < infectProb:
+                        toInfect.add(student)
 
         toInfect = list(toInfect)
         newlyInfected += len(toInfect)
@@ -49,6 +95,7 @@ def infectPeriod(infoDict, classrooms, infectedList, baseRate, baseEnvRate):
         nextPeriod.extend(toInfect)
 
     return nextPeriod, newlyInfected/len(infectedList)
+
 
 def augmentProb(baseRate, student):
 
